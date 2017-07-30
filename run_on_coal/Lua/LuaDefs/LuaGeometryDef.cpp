4edf6d0b22b2c6f7884968c05eb1efc1814652a8
SDraw <sdraw5130@gmail.com>
2017-07-30 09:48:17 +0300

    Client: Added 'isLoaded' method for Geometry Lua definitions

--- run_on_coal/Lua/LuaDefs/LuaGeometryDef.cpp
+++ run_on_coal/Lua/LuaDefs/LuaGeometryDef.cpp
@@ -15,6 +15,7 @@ void ROC::LuaGeometryDef::Init(lua_State *f_vm)
 {
     LuaUtils::lua_registerClass(f_vm, "Geometry", GeometryCreate);
     LuaUtils::lua_registerClassMethod(f_vm, "getBoundSphereRadius", GeometryGetBoundSphereRadius);
+    LuaUtils::lua_registerClassMethod(f_vm, "isLoaded", GeometryIsLoaded);
     LuaElementDef::AddHierarchyMethods(f_vm);
     LuaUtils::lua_registerClassFinish(f_vm);
 }
@@ -32,6 +33,15 @@ int ROC::LuaGeometryDef::GeometryCreate(lua_State *f_vm)
         l_geometry ? argStream.PushElement(l_geometry) : argStream.PushBoolean(false);
     }
     else argStream.PushBoolean(false);
+    return argStream.GetReturnValue();
+}
+
+int ROC::LuaGeometryDef::GeometryIsLoaded(lua_State *f_vm)
+{
+    Geometry *l_geometry;
+    ArgReader argStream(f_vm);
+    argStream.ReadElement(l_geometry);
+    !argStream.HasErrors() ? argStream.PushNumber(l_geometry->IsLoaded()) : argStream.PushBoolean(false);
     return argStream.GetReturnValue();
 }
 
--- run_on_coal/Lua/LuaDefs/LuaGeometryDef.h
+++ run_on_coal/Lua/LuaDefs/LuaGeometryDef.h
@@ -6,6 +6,7 @@ namespace ROC
 class LuaGeometryDef final
 {
     static int GeometryCreate(lua_State *f_vm);
+    static int GeometryIsLoaded(lua_State *f_vm);
     static int GeometryGetBoundSphereRadius(lua_State *f_vm);
 protected:
     static void Init(lua_State *f_vm);
